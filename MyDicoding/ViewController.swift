b7c9d899bc226b44f141135a186808e603541a3a
Aimmzz <rohimkurniawan456@gmail.com>
2023-12-06 20:58:43 +0700

    Initial Commit

---
+++ MyDicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDicoding
+//
+//  Created by rohim kurniawan on 06/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
